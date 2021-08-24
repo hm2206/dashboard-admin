f16d5ad1f954c71c0e5305e01e5aecae67c69c61
Hans <twd2206@gmail.com>
2021-08-23 22:21:37 -0500

    primer commit

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index = () => {
+    return 'page Index'
+}
+
+export default Index;
---
+++ pages/login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import FormLogin from '../components/auth/formLogin';
+
+const PageLogin = () => {
+
+    return (
+        <Container fluid={true} className="p-0">
+            <Row className="m-0">
+                <Col xs="12" className="p-0">     
+                    <FormLogin/>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default PageLogin;
