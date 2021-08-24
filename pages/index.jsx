179745433f2e163b5845cebe6ac854afbe6168cf
Hans <twd2206@gmail.com>
2021-08-24 00:05:13 -0500

    estructura del main dashboard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Sidebar from '../components/sidebar'
+import Header from '../components/header';
+import DefaultLayout from '../components/layouts/defaultLayout'
 
 const Index = () => {
-    return 'page Index'
+    return (
+        <DefaultLayout
+            sidebar={<Sidebar/>}
+            header={<Header/>}
+        >
+            hkafdgasgsg
+        </DefaultLayout>
+    )
 }
 
 export default Index;
