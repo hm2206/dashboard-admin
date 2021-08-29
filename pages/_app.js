543af37b20f8f9ea926234c31cdcd1b525825ffa
Hans <twd2206@gmail.com>
2021-08-29 11:31:10 -0500

    fix font-google

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,12 @@ const MyApp = ({ Component, pageProps }) => {
   return (
       <PersistGate persistor={store.__PERSISTOR}>
         <Head>
+          {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap"/>
+          {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap"/>
+          {/* eslint-disable-next-line @next/next/no-css-tags */}
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css"/>
           <title>{pageProps?.props?.title}</title>
         </Head>
 
